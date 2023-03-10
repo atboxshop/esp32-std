82c38b26c705b01d0b93b1172c61cc2360bc543b
atboxshop <atboxshop@gmail.com>
2023-03-10 02:07:38 -0500

    123

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ fn main()
     loop {
         led.set_high().unwrap();
         // we are sleeping here to make sure the watchdog isn't triggered
-        FreeRtos::delay_ms(500);
+        FreeRtos::delay_ms(200);
         led.set_low().unwrap();
-        FreeRtos::delay_ms(500);
+        FreeRtos::delay_ms(200);
         println!("Hello World! 123");
    }
 }
