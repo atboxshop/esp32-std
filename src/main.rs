0ba12315f282d72a0a54c9d935c12e662485773c
atboxshop <atboxshop@gmail.com>
2023-03-13 09:11:47 -0400

    456

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,46 @@ use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, alway
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_svc::{
+    wifi::EspWifi,
+    nvs::EspDefaultNvsPartition,
+    eventloop::EspSystemEventLoop,
+};
+use embedded_svc::wifi::{ClientConfiguration, Wifi, Configuration};
 
 fn main()
 {
     let peripherals = Peripherals::take().unwrap();
-    let mut led = PinDriver::output(peripherals.pins.gpio5).unwrap();
     let mut led2 = PinDriver::output(peripherals.pins.gpio2).unwrap();
+    let sys_loop = EspSystemEventLoop::take().unwrap();
+    let nvs = EspDefaultNvsPartition::take().unwrap();
 
+    let mut wifi_driver = EspWifi::new(
+        peripherals.modem,
+        sys_loop,
+        Some(nvs)
+    ).unwrap();
+
+    wifi_driver.set_configuration(&Configuration::Client(ClientConfiguration{
+        ssid: "FTW123".into(),
+        password: "hongphat0106".into(),
+        ..Default::default()
+    })).unwrap();
+
+    wifi_driver.start().unwrap();
+    wifi_driver.connect().unwrap();
+    while !wifi_driver.is_connected().unwrap(){
+        let config = wifi_driver.get_configuration().unwrap();
+        println!("Waiting for station {:?}", config);
+    }
+    println!("Should be connected now");
 
     loop{
-        led.set_high().unwrap();
+        println!("IP info: {:?}", wifi_driver.sta_netif().get_ip_info().unwrap());
         led2.set_high().unwrap();
         // we are sleeping here to make sure the watchdog isn't triggered
         FreeRtos::delay_ms(500);
-        led.set_low().unwrap();
         led2.set_low().unwrap();
         FreeRtos::delay_ms(500);
-        println!("Hello World!");
    }
 }
